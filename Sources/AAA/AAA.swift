9c7ff4dc84c50fc78dc899feada75a1797a72d62
Mayu Laierlence <minacle@live.com>
2016-12-13 16:04:07 +0900

    expressible by literals

--- Sources/AAA/AAA.swift
+++ Sources/AAA/AAA.swift
@@ -1,11 +1,14 @@
-import Foundation
+import Swift
 
-/// An Any Accessory
+/// An Any Accessory.
 public struct AAA {
-
     private let o: Any?
 
-    public init(_ o: Any? = nil) {
+    public init() {
+        self.o = nil
+    }
+
+    public init(_ o: Any?) {
         self.o = o
     }
 
@@ -25,5 +28,66 @@ public struct AAA {
             }
         }
         return AAA()
+    }
+}
+
+extension AAA: ExpressibleByArrayLiteral {
+
+    public init(arrayLiteral elements: Any...) {
+        self.init(elements)
+    }
+}
+
+extension AAA: ExpressibleByBooleanLiteral {
+
+    public init(booleanLiteral value: BooleanLiteralType) {
+        self.init(value)
+    }
+}
+
+extension AAA: ExpressibleByDictionaryLiteral {
+
+    public init(dictionaryLiteral elements: (AnyHashable, Any)...) {
+        var d = [AnyHashable: Any]()
+        for (k, v) in elements {
+            d[k] = v
+        }
+        self.init(d)
+    }
+}
+
+extension AAA: ExpressibleByFloatLiteral {
+
+    public init(floatLiteral value: FloatLiteralType) {
+        self.init(value)
+    }
+}
+
+extension AAA: ExpressibleByIntegerLiteral {
+
+    public init(integerLiteral value: IntegerLiteralType) {
+        self.init(value)
+    }
+}
+
+extension AAA: ExpressibleByNilLiteral {
+
+    public init(nilLiteral _: ()) {
+        self.init()
+    }
+}
+
+extension AAA: ExpressibleByStringLiteral {
+
+    public init(extendedGraphemeClusterLiteral value: StringLiteralType) {
+        self.init(value)
+    }
+
+    public init(stringLiteral value: StringLiteralType) {
+        self.init(value)
+    }
+
+    public init(unicodeScalarLiteral value: StringLiteralType) {
+        self.init(value)
     }
 }
